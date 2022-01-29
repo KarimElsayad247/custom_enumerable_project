b3e4173d7c1e2ce43342a99bd365cf4fd35db81b
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-01-29 22:53:24 +0200

    Implement inject

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -29,7 +29,7 @@ module Enumerable
       if block_given?
         total += 1 if block.call(obj)
       else
-        if item != nil
+        if item.nil?
           total += 1 if obj == item
         else
           total += 1
@@ -39,10 +39,24 @@ module Enumerable
     return total
   end
 
-  def my_inject
+  def my_inject(initial=nil, &block)
+    if initial.nil?
+      result = at(0)
+    else
+      result = initial
+    end
+    my_each do |obj|
+      result = block.call(result, obj)
+    end
+    return result
   end
 
-  def my_map
+  def my_map(&block)
+    result = []
+    my_each do |obj|
+      result << block.call(obj)
+    end
+    return result
   end
 
   def my_none?(&block)
